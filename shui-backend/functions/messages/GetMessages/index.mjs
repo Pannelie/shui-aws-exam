de5f6ada42c220ee01d2aba6719469bb60b1234d
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 14:07:13 +0200

    removed body parser middleware for GET, it does not need a body, so its unnceessary and creates error

--- shui-backend/functions/messages/GetMessages/index.mjs
+++ shui-backend/functions/messages/GetMessages/index.mjs
@@ -1,4 +1,20 @@
 import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { getMessages } from "../../../services/messages.mjs";
+import { formatMessageResponse } from "../../../responses/index.mjs";
+import { sendResponse } from "../../../responses/index.mjs";
+export const handler = middy(async (event) => {
+  const messages = await getMessages();
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+  if (!messages || messages.length === 0) {
+    return sendResponse(200, {
+      success: true,
+      message: `No messages to show`,
+    });
+  }
+
+  return sendResponse(200, {
+    success: true,
+    message: `These are the avaiable messages`,
+    messages: messages.map(formatMessageResponse),
+  });
+});
