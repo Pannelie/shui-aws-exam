eee9b9e506639283942a0e272fba9f63d919a926
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 13:33:22 +0200

    import and use of addMessage inside handler for PostMessage

--- shui-backend/functions/messages/PostMessage/index.mjs
+++ shui-backend/functions/messages/PostMessage/index.mjs
@@ -1,4 +1,17 @@
 import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { addMessage } from "../../../services/messages.mjs";
+import { formatMessageResponse, sendResponse } from "../../../responses/index.mjs";
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+export const handler = middy(async (event) => {
+  const message = await addMessage(event.body);
+  if (!message.success) {
+    console.error(`Could not create message`);
+    return sendResponse(400, { success: false, message: `Could not create message` });
+  }
+  return sendResponse(201, {
+    success: true,
+    message: `Successfully posted message`,
+    note: formatMessageResponse(message),
+  });
+}).use(httpJsonBodyParser());
