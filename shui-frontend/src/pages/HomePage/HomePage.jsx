001c9ba60702e6a9420b288896c549041481568f
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 10:46:28 +0200

    HomePage in pages

---
+++ shui-frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../../components/NavBar/NavBar";
+import "./homePage.css";
+
+export const HomePage = () => {
+  return (
+    <section className="home-page">
+      HomePage
+      <NavBar />
+    </section>
+  );
+};
