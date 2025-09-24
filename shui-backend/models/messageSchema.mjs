6b1899a75e16d5c2c734f624ca323b1ef9b6b93b
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 12:03:36 +0200

    removed id and createAt from schema

--- shui-backend/models/messageSchema.mjs
+++ shui-backend/models/messageSchema.mjs
@@ -1,13 +1,6 @@
 import Joi from "joi";
-import { generateShortId } from "../utils/generateShortId.mjs";
 
 export const messageSchema = Joi.object({
-  id: Joi.string()
-    .forbidden()
-    .default(() => generateShortId(), "auto-generated UUID"),
   username: Joi.string().min(3).required(),
   text: Joi.string().min(2).required(),
-  createdAt: Joi.string()
-    .forbidden()
-    .default(() => new Date().toISOString(), "current timestamp"),
 });
