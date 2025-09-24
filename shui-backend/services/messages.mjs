9932b4763b0f1e81278a93329a36803a5562f736
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 22:08:10 +0200

    created getMessagesByUser

--- shui-backend/services/messages.mjs
+++ shui-backend/services/messages.mjs
@@ -21,6 +21,26 @@ export const getMessages = async () => {
   }
 };
 
+export const getMessagesByUser = async (username) => {
+  try {
+    const command = new QueryCommand({
+      TableName: "shui-table",
+      IndexName: "GSI1",
+      KeyConditionExpression: "GSI1PK= :username",
+      ExpressionAttributeValues: {
+        ":username": `USER#${username}`,
+      },
+      ScanIndexForward: true, // true innebär äldsta först
+    });
+
+    const result = await docClient.send(command);
+    return result.Items || [];
+  } catch (error) {
+    console.error(`Error fetching messages for user ${username}:`, error.message);
+    throwError("Could not fetch messages for user", 500);
+  }
+};
+
 export const addMessage = async ({ username, text }) => {
   const messageId = generateId();
   const createdAt = new Date().toISOString();
