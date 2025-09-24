a150ae46f7834509b2152add4de256bfc6ef21f3
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 21:17:03 +0200

    created DeleteMessageById function

--- shui-backend/functions/messages/DeleteMessageById/index.mjs
+++ shui-backend/functions/messages/DeleteMessageById/index.mjs
@@ -1,4 +1,21 @@
 import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { throwError } from "../../../responses/throwError.mjs";
+import { errorHandler } from "../../../middlewares/errorHandler.mjs";
+import { formatMessageResponse, sendResponse } from "../../../responses/index.mjs";
+import { deleteMessage } from "../../../services/messages.mjs";
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+export const handler = middy(async (event) => {
+  const messageId = event.pathParameters?.id;
+
+  if (!messageId) {
+    throwError("Missing messageId in path parameters", 400); //400 = bad request
+  }
+
+  const result = await deleteMessage(messageId);
+
+  if (!result.success) {
+    throwError(result.message || "Failed to update message", 500);
+  }
+
+  return sendResponse(200, { message: "Message deleted successfully!", deletedMessage: formatMessageResponse(result.deletedMessage) });
+}).use(errorHandler());
