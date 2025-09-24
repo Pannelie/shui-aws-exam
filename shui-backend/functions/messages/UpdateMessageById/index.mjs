25d829f696e567dc5e41d5abf0d052fd132a6bf4
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 15:41:24 +0200

    startcode, time for break

--- shui-backend/functions/messages/UpdateMessageById/index.mjs
+++ shui-backend/functions/messages/UpdateMessageById/index.mjs
@@ -1,4 +1,27 @@
 import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { throwError } from "../../../responses/throwError.mjs";
+import { validateMessageUpdate } from "../../../middlewares/validateMessageUpdate.mjs";
+import { errorHandler } from "../../../middlewares/errorHandler.mjs";
+import { updateMessage } from "../../../services/messages.mjs";
+import { formatMessageResponse, sendResponse } from "../../../responses/index.mjs";
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+export const handler = middy(async (event) => {
+  const messageId = event.pathParameters?.id;
+  const updatedText = event.body;
+
+  if (!messageId) {
+    throwError("Missing messageId in path parameters", 400); //400 = bad request
+  }
+
+  const result = await updateMessage(messageId, updatedText);
+
+  if (!result.success) {
+    throwError(result.message || "Failed to update message", 500);
+  }
+
+  return sendResponse(200, { message: "Message updated successfully!", updatedMessage: formatMessageResponse(result.updatedMessage) });
+})
+  .use(httpJsonBodyParser())
+  .use(validateMessageUpdate())
+  .use(errorHandler());
