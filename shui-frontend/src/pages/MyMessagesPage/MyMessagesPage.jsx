461d3d84188cea1941523dd5229b2ba5593d398e
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 10:46:36 +0200

    MyMessagesPage in pages

---
+++ shui-frontend/src/pages/MyMessagesPage/MyMessagesPage.jsx
@@ -0,0 +1,11 @@
+import "./myMessagesPage.css";
+import { NavBar } from "../../components/NavBar/NavBar";
+
+export const MyMessagesPage = () => {
+  return (
+    <section className="messages-page">
+      MyMessagesPage
+      <NavBar />
+    </section>
+  );
+};
