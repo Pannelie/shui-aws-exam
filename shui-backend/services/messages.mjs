b0848caec9a321593dbc33b2ed4bac879447b362
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 13:34:39 +0200

    added Putcommand for posting message into dynamodb

--- shui-backend/services/messages.mjs
+++ shui-backend/services/messages.mjs
@@ -1,3 +1,30 @@
-import { client } from "./client.mjs";
+import { generateId } from "../utils/generateId.mjs";
+import { docClient } from "./client.mjs";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
-export const addMessage = async ({ username, text, createdAt }) => {};
+export const addMessage = async ({ username, text }) => {
+  const messageId = generateId();
+  const createdAt = new Date().toISOString();
+
+  const item = {
+    PK: "MESSAGE",
+    SK: `CREATED_AT#${createdAt}`,
+    messageId,
+    username,
+    text,
+    createdAt,
+  };
+
+  const command = new PutCommand({
+    TableName: "shui-table",
+    Item: item,
+  });
+
+  try {
+    await docClient.send(command);
+    return { success: true, ...item };
+  } catch (error) {
+    console.error(`Error från db:`, error);
+    return { success: false, message: `Error saving booking: ${error.message}` };
+  }
+};
