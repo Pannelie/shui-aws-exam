ab1258b98b4c2d63b531bc42647b8e6a16303a4a
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 15:40:11 +0200

    created GetMessagesById, import  middleware, checking if pathParameters etc.

--- shui-backend/functions/messages/GetMessageById/index.mjs
+++ shui-backend/functions/messages/GetMessageById/index.mjs
@@ -1,4 +1,21 @@
 import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { throwError } from "../../../responses/throwError.mjs";
+import { errorHandler } from "../../../middlewares/errorHandler.mjs";
+import { updateMessage } from "../../../services/messages.mjs";
+import { formatMessageResponse } from "../../../responses/index.mjs";
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+export const handler = middy(async (event) => {
+  const messageId = event.pathParameters?.id;
+
+  if (!messageId) {
+    throwError("Missing messageId in path parameters", 400); //400 = bad request
+  }
+
+  const result = await updateMessage(messageId, updatedText);
+
+  if (!result.success) {
+    throwError(result.message || "Failed to update message", 500);
+  }
+
+  return sendResponse(200, { message: "Message updated successfully!", updatedMessage: formatMessageResponse(result.updatedMessage) });
+}).use(errorHandler());
