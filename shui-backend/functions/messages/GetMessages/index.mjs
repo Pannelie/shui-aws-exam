abaf9077176e6eae33929b1fd02a39b1844e17e0
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-24 22:08:00 +0200

    added formatCountMessage, and removed code that overflowed

--- shui-backend/functions/messages/GetMessages/index.mjs
+++ shui-backend/functions/messages/GetMessages/index.mjs
@@ -3,11 +3,14 @@ import { getMessages } from "../../../services/messages.mjs";
 import { formatMessageResponse } from "../../../responses/index.mjs";
 import { sendResponse } from "../../../responses/index.mjs";
 import { errorHandler } from "../../../middlewares/errorHandler.mjs";
+import { formatCountMessage } from "../../../utils/formatCountMessage.mjs";
 
 export const handler = middy(async (event) => {
   const messages = await getMessages();
 
-  if (!messages || messages.length === 0) {
+  const count = messages?.length || 0;
+
+  if (count === 0) {
     return sendResponse(200, {
       success: true,
       message: `No messages to show`,
@@ -16,7 +19,7 @@ export const handler = middy(async (event) => {
 
   return sendResponse(200, {
     success: true,
-    message: `These are the avaiable messages`,
+    message: formatCountMessage(count, "message"),
     messages: messages.map(formatMessageResponse),
   });
 }).use(errorHandler());
