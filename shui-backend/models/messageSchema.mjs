142103639aae9f08b8977bcc5a2b8be4f70665fb
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-25 16:37:01 +0200

    Realised that username is unneccessary. username is generated outside of the body

--- shui-backend/models/messageSchema.mjs
+++ shui-backend/models/messageSchema.mjs
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const messageSchema = Joi.object({
-  username: Joi.string().min(3).required(),
+  // username: Joi.string().min(3).required(),
   text: Joi.string().min(2).required(),
 });
