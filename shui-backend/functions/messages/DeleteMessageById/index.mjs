2f70f71122241b3aa69ccac80253e178cd9405e6
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-25 16:32:03 +0200

    changed DeleteMessage handler. Controlls if its the right user before deleting message. Also imorted new service, getMessageById

--- shui-backend/functions/messages/DeleteMessageById/index.mjs
+++ shui-backend/functions/messages/DeleteMessageById/index.mjs
@@ -2,7 +2,9 @@ import middy from "@middy/core";
 import { throwError } from "../../../responses/throwError.mjs";
 import { errorHandler } from "../../../middlewares/errorHandler.mjs";
 import { formatMessageResponse, sendResponse } from "../../../responses/index.mjs";
-import { deleteMessage } from "../../../services/messages.mjs";
+import { deleteMessage, getMessageById } from "../../../services/messages.mjs";
+import { authenticateUser } from "../../../middlewares/authenticateUser.mjs";
+import { authorizeRole } from "../../../middlewares/authorizeRole.mjs";
 
 export const handler = middy(async (event) => {
   const messageId = event.pathParameters?.id;
@@ -11,11 +13,19 @@ export const handler = middy(async (event) => {
     throwError("Missing messageId in path parameters", 400); //400 = bad request
   }
 
-  const result = await deleteMessage(messageId);
+  const message = await getMessageById(messageId);
 
-  if (!result.success) {
-    throwError(result.message || "Failed to update message", 500);
+  if (message.username !== event.user.username) {
+    throwError("Forbidden: Cannot delete someone else's message", 403);
   }
 
-  return sendResponse(200, { message: "Message deleted successfully!", deletedMessage: formatMessageResponse(result.deletedMessage) });
-}).use(errorHandler());
+  const deleteResult = await deleteMessage(messageId);
+
+  return sendResponse(200, {
+    message: "Message deleted successfully!",
+    deletedMessage: formatMessageResponse(deleteResult.deletedMessage),
+  });
+})
+  .use(authenticateUser())
+  .use(authorizeRole(["USER"]))
+  .use(errorHandler());
