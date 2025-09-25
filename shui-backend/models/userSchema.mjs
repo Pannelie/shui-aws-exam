cbdc5a9c1e452c8c62df6c9defa2bdf6d7a93130
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-25 16:38:50 +0200

    shows how a user must look when created. Contains username, password and email. Role is enlisted, not chosen by user

---
+++ shui-backend/models/userSchema.mjs
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export const userSchema = Joi.object({
+  username: Joi.string().min(3).required(),
+  password: Joi.string()
+    .alphanum()
+    .min(4)
+    .pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/)
+    .messages({
+      "string.pattern.base": "Lösenordet måste innehålla minst en stor bokstav, en liten bokstav och en siffra.",
+    })
+    .required(),
+  email: Joi.string().email().required(),
+  role: Joi.string().forbidden(),
+  //jag vill inte att användaren själv väljer role
+});
