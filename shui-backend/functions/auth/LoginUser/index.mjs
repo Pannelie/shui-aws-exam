f703f004ca91705908d4cab43c2c3b25717715d5
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-25 16:29:11 +0200

    created handler for login. gets user from services and compares password. import generateToken and adds it to the response.

--- shui-backend/functions/auth/LoginUser/index.mjs
+++ shui-backend/functions/auth/LoginUser/index.mjs
@@ -1,4 +1,32 @@
 import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { sendResponse } from "../../../responses/index.mjs";
+import { throwError } from "../../../responses/throwError.mjs";
+import { getUser } from "../../../services/users.mjs";
+import { errorHandler } from "../../../middlewares/errorHandler.mjs";
+import { validateLogin } from "../../../middlewares/validateLogin.mjs";
+import { generateToken } from "../../../utils/jwt.mjs";
+import { comparePasswords } from "../../../utils/bcrypt.mjs";
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+export const handler = middy(async (event) => {
+  const response = await getUser(event.body.username);
+  if (!response) {
+    throwError("User not found", 404);
+  }
+
+  const passwordValid = await comparePasswords(event.body.password, response.password);
+  if (!passwordValid) {
+    throwError("Wrong password", 401);
+  }
+  const token = generateToken({ username: response.username, role: response.role });
+
+  return sendResponse(200, {
+    success: true,
+    message: `${response.username} logged in successfully`,
+    role: response.role,
+    token: `Bearer ${token}`,
+  });
+})
+  .use(httpJsonBodyParser())
+  .use(validateLogin())
+  .use(errorHandler());
