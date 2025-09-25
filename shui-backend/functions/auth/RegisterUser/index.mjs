99e10ac7947b541cdcd30e3f1d827c2d8f51edd5
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-25 16:30:30 +0200

    created registeruser. Adds role, default is USER. if not successfull response --> throws error with according error message based on response.error

--- shui-backend/functions/auth/RegisterUser/index.mjs
+++ shui-backend/functions/auth/RegisterUser/index.mjs
@@ -1,4 +1,28 @@
 import middy from "@middy/core";
 import httpJsonBodyParser from "@middy/http-json-body-parser";
+import { addUser } from "../../../services/users.mjs";
+import { sendResponse } from "../../../responses/index.mjs";
+import { validateUser } from "../../../middlewares/validateUser.mjs";
+import { errorHandler } from "../../../middlewares/errorHandler.mjs";
+import { throwError } from "../../../responses/throwError.mjs";
 
-export const handler = middy(async (event) => {}).use(httpJsonBodyParser);
+export const handler = middy(async (event) => {
+  const userData = {
+    ...event.body,
+    role: event.body.role || "USER", // default till USER
+  };
+  console.log("Checking if user exists with username:", userData.username);
+
+  const response = await addUser(userData);
+  //response=== true om lyckat
+
+  if (!response.success) {
+    const status = response.message === "User already exists" ? 400 : 500;
+    throwError(response.message, status);
+  }
+
+  return sendResponse(201, { message: "User created successfully" });
+})
+  .use(httpJsonBodyParser())
+  .use(validateUser())
+  .use(errorHandler());
