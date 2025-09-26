173bee59b7f00c0025cd70b51a9d401ec267fc2d
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-26 09:52:04 +0200

    changed into english error message for consistency

--- shui-backend/models/userSchema.mjs
+++ shui-backend/models/userSchema.mjs
@@ -7,7 +7,7 @@ export const userSchema = Joi.object({
     .min(4)
     .pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/)
     .messages({
-      "string.pattern.base": "Lösenordet måste innehålla minst en stor bokstav, en liten bokstav och en siffra.",
+      "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter and one number.",
     })
     .required(),
   email: Joi.string().email().required(),
