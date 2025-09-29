0a5112714e8a859f85f1ea90528f8b7e79ad2abc
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-29 14:34:32 +0200

    added handleSubmit, if there is inititaldata, then you will see it in textarea

--- shui-frontend/src/components/MessageForm/MessageForm.jsx
+++ shui-frontend/src/components/MessageForm/MessageForm.jsx
@@ -1,5 +1,12 @@
 import "./messageForm.css";
+import { useState } from "react";
 
 export const MessageForm = ({ initialData = {}, onSubmit }) => {
+  const [text, setText] = useState(initialData.text || "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ ...initialData, text });
+  };
   return <form></form>;
 };
