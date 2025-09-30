43ca2411cb278eb576e3f36db6d29c22a3ca8c4a
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-09-30 12:58:17 +0200

    created button for writing new Message

--- shui-frontend/src/components/WriteButton/WriteButton.jsx
+++ shui-frontend/src/components/WriteButton/WriteButton.jsx
@@ -1,11 +1,20 @@
 import "./writeButton.css";
-import PenIcon from "../../assets/vectors/VectorpenIcon.svg";
+// import PenIcon from "../../assets/vectors/VectorpenIcon.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 
-export const WriteButton = ({ onClick, text }) => {
+import { useNavigate } from "react-router-dom";
+
+export const WriteButton = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/messages/write"); // byt "/write" till din route för att skriva nytt meddelande
+  };
   return (
-    <button className="write-button" onClick={onClick}>
-      <img src={PenIcon} alt="Pen icon" className="write-button__icon" />
-      {text}
+    <button className="write-button" onClick={handleClick}>
+      <FontAwesomeIcon icon={faPen} className="write-button__icon" />
+
+      {/* <img src={PenIcon} alt="Skriv nytt meddelande" className="write-button__icon" /> */}
     </button>
   );
 };
