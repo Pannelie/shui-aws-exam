a45cf65611e8d87ec3c49918754a67cd76716265
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-01 22:48:20 +0200

    added props activeSort and onToggle

--- shui-frontend/src/components/Header/Header.jsx
+++ shui-frontend/src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import { LogoutButton } from "../LogoutButton/LogoutButton";
 import { MessageSwitch } from "../MessageSwitch/MessageSwitch";
 import "./header.css";
 
-export const Header = ({ view, setView, showSwitch = true }) => {
+export const Header = ({ view, setView, showSwitch = true, activeSort, onToggle }) => {
   return (
     <section className={`header ${showSwitch ? "header--full" : "header--compact"}`}>
       <Logo />
       <LogoutButton />
-      {showSwitch && view && setView && <MessageSwitch view={view} setView={setView} />}
+      {showSwitch && view && setView && <MessageSwitch view={view} setView={setView} activeSort={activeSort} onToggle={onToggle} />}
     </section>
   );
 };
