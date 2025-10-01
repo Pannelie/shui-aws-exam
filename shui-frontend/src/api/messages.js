a776ddffc837195c90fbff87f247fe8ab77875c7
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-10-01 21:33:49 +0200

    bugfix

--- shui-frontend/src/api/messages.js
+++ shui-frontend/src/api/messages.js
@@ -4,7 +4,7 @@ const baseURL = "https://t0woxk6mb6.execute-api.eu-north-1.amazonaws.com";
 
 export const getMessagesApi = async (token, type = "all") => {
   if (!token) return { success: false, message: "Ingen token tillgänglig" };
-  console.log(`Detta är token: ${token}`);
+  // console.log(`Detta är token: ${token}`);
 
   return await axios
     .get(`${baseURL}/api/messages/type/${type}`, {
@@ -21,6 +21,24 @@ export const getMessagesApi = async (token, type = "all") => {
         message: error.response?.data?.message || "Något gick fel",
       };
     });
+};
+export const getMessagesByUserApi = async (token) => {
+  if (!token) return { success: false, message: "Ingen token tillgänglig" };
+
+  try {
+    const response = await axios.get(`${baseURL}/api/messages/type/mine`, {
+      headers: {
+        Authorization: token.startsWith("Bearer ") ? token : `Bearer ${token}`,
+      },
+    });
+
+    return { success: true, data: response.data };
+  } catch (error) {
+    return {
+      success: false,
+      message: error.response?.data?.message || `Kunde inte hämta meddelanden för ${username}`,
+    };
+  }
 };
 
 export const getMessageByIdApi = async (messageId, token) => {
